c50b4e0f37406cbd2a187de6976a0165b34bc8f8
邱榆洋 <qiuyuyang@Fisheep.local>
2020-07-25 14:54:34 +0800

    Final

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let port = process.env.port || 8888
+let port = process.env.PORT || 8888
 let user_router = require('./routers/main_router.js')
 
 app.get("/", (req, res) => {
